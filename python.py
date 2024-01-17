52b5c260b80d4d332ab4ef44cc362ea98f0ccd95
Muhammad Ishaq <156215583+muhammadiishaq@users.noreply.github.com>
2024-01-17 21:24:14 +0500

    Add files via upload

---
+++ python.py
@@ -0,0 +1,66 @@
+# Muhammad ishaq
+
+# muuhammadishaqpak801@gmail.com
+
+# Task: 1
+
+# Write a Python program to do arithmetical operations addition and division.
+
+# Part : A
+
+add_num1 = int(input("Enter the first number for addition : "))
+add_num2 = int(input("Enter the second number for addition : "))
+total_sum = add_num1 + add_num2
+print(f"Sum: {add_num1} + {add_num2} = {total_sum}")
+
+# Part : B
+
+# Another question for division .
+
+
+div_num1 = int(input("Enter the first number for division : "))
+div_num2 = int(input("Enter the second number for division : "))
+division = div_num1 / div_num2
+print(f"Sum: {div_num1} + {div_num2} = {division}")
+
+# Task:2
+
+# Write a Python program to find the area of a triangle.
+
+b = int(input("Enter the length of the base of the traingle : "))
+h = int(input("Enter the height of the traingle : "))
+
+area_formula = 1/2*b*h
+print("The area of the traingle is : ", area_formula)
+
+# Task : 3
+
+# Write a Python program to convert Celsius to Fahrenheit.
+
+celsi_num = float(input("Enter temperature in Celsius : "))
+
+F = celsi_num*(9/5)+32
+print(f"{celsi_num} degrees Celsius is equle to {F} degrees Fahrenheit")
+
+# Task : 4
+
+# Write a Python program that return all datatypes that we discussed in the class.
+
+a = 19
+print(f"The Data Type of {a} is =",type(a))
+
+f = 45.0
+print(f"The Data Type of {f} is =",type(f))
+
+b = "Ishaq"
+print(f"The Data Type of my name is {b} =",type(b))
+
+d = True
+print(f"The Data Type of {d} is =",type(d))
+
+
+
+
+
+
+
